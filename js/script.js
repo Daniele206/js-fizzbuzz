e2835ca7d3613f4367235fd85883907c29144459
Daniele206 <danieledeieso2004@gmail.com>
2024-02-22 16:06:13 +0100

    [add] fizz,buzz,fizzbuzz too HTML

--- js/script.js
+++ js/script.js
@@ -3,15 +3,30 @@ const limit = 100;
 const m3 = 3;
 const m5 = 5;
 
+// output
+const myList = document.getElementById('my-list');
+
 // ciclo
 for(let i = 1; i <= limit; i++){
   if(!(i % m3) && (i % m5)){
     console.log('fizz');
+    myList.innerHTML +=`
+    <li>fizz</li>
+    `;
   } else if((i % m3) && !(i % m5)){
     console.log('buzz');
+    myList.innerHTML +=`
+    <li>buzz</li>
+    `;
   } else if(!(i % m3) && !(i % m5)){
     console.log('fizzbuzz');
+    myList.innerHTML +=`
+    <li>fizzbuzz</li>
+    `;
   } else{
     console.log(i);
+    myList.innerHTML +=`
+    <li>${i}</li>
+    `;
   }
 }
