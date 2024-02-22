c909c61aea372f6cf47f2dc37dfc0e5ee17f9a7b
Daniele206 <danieledeieso2004@gmail.com>
2024-02-22 16:24:32 +0100

    [add] different stile too HTML-elements

--- js/script.js
+++ js/script.js
@@ -11,22 +11,22 @@ for(let i = 1; i <= limit; i++){
   if(!(i % m3) && (i % m5)){
     console.log('fizz');
     myList.innerHTML +=`
-    <li>fizz</li>
+    <li class="my-square fizz">fizz</li>
     `;
   } else if((i % m3) && !(i % m5)){
     console.log('buzz');
     myList.innerHTML +=`
-    <li>buzz</li>
+    <li class="my-square buzz">buzz</li>
     `;
   } else if(!(i % m3) && !(i % m5)){
     console.log('fizzbuzz');
     myList.innerHTML +=`
-    <li>fizzbuzz</li>
+    <li class="my-square fizz-buzz">fizzbuzz</li>
     `;
   } else{
     console.log(i);
     myList.innerHTML +=`
-    <li>${i}</li>
+    <li class="my-square my-element">${i}</li>
     `;
   }
 }
