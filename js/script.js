306cecbed00eb40471b082d26a595bd5f3840fd7
Daniele206 <danieledeieso2004@gmail.com>
2024-02-22 15:50:49 +0100

    [add] 1to100  with fizz,buzz,fizzbuzz (consol.log)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // costanti
-const limit = 100;
+const limit = 101;
 const m3 = 3;
 const m5 = 5;
 
 // ciclo
-for(let i = 1; i <= limit; i++){
-  console.log(i);
+for(let i = 1; i < limit; i++){
+  if(!(i % m3) && (i % m5)){
+    console.log('fizz');
+  } else if((i % m3) && !(i % m5)){
+    console.log('buzz');
+  } else if(!(i % m3) && !(i % m5)){
+    console.log('fizzbuzz');
+  } else{
+    console.log(i);
+  }
 }
